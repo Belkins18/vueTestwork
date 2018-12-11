de195d9e0a726fb974f4da28f996a311d013f7d1
Belkins <nikolay.belibov@gmail.com>
2018-12-11 17:28:01 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/router';
+import store from './store/store';
+
+import Loading from './components/lib/loading';
+import CenterContainer from './components/lib/center-container';
 
 Vue.config.productionTip = false;
+Vue.component('loading', Loading);
+Vue.component('center-container', CenterContainer);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	store,
+	render: h => h(App),
+}).$mount('#app');
